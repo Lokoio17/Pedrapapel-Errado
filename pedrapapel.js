ee439f6ef4c031221c432136ab41891614d6f2c8
Lokoio17 <100881061+Lokoio17@users.noreply.github.com>
2022-09-27 16:15:20 -0300

    Add files via upload
    Teste err

---
+++ pedrapapel.js
@@ -0,0 +1,62 @@
+let pontuacaoPessoa = 0;
+let pontuacaoMaquina = 0;
+
+const resultado = document.getElementById("resultado");
+
+const jogadaP = document.querySelector("input[name='opcao']:checked").val().toLowerCase();
+
+const jogadaM = () => {
+  const escolhaM = ["Pedra", "Papel", "Tesoura"];
+  return escolhaM[Math.floor(Math.random() * escolhaM.length)].toLowerCase();
+};
+
+const vitoriaM = () => {
+  return (
+    ++pontuacaoMaquina + resultado.innerText("A maquina venceu")
+  );
+};
+const vitoriaP = () => {
+  return ++pontuacaoPessoa + resultado.innerText("Voce venceu");
+};
+
+const rodada= () => {
+  switch (jogadaP && jogadaM()) {
+    case jogadaP == jogadaM:
+      {
+        resultado.innerText("O jogo deu empate");
+      }
+      break;
+    case JogadaP == "pedra" && jogadaM() == "tesoura":
+      {
+        vitoriaP();
+      }
+      break;
+    case jogadaP == "papel" && jogadaM() == "pedra":
+      {
+        vitoriaP();
+      }
+      break;
+    case jogadaP == "tesoura" && jogadaM() == "papel":
+      {
+        vitoriaP();
+      }
+      break;
+    case jogadaM() == "pedra" && jogadaP == "tesoura":
+      {
+        vitoriaM();
+      }
+      break;
+    case jogadaM() == "papel" && jogadaP == "pedra":
+      {
+        vitoriaM();
+      }
+      break;
+    case jogadaM() == "tesoura" && jogadaP == "papel":
+      {
+        vitoriaM();
+      }
+      break;
+  }
+};
+const botao = document.getElementById("botao");
+botao.addEventListener("click",rodada);
