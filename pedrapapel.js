49b556d168613eff9862041c67973d3e7698df7c
Lokoio17 <100881061+Lokoio17@users.noreply.github.com>
2022-09-29 13:44:12 -0300

    Update pedrapapel.js

--- pedrapapel.js
+++ pedrapapel.js
@@ -1,60 +1,43 @@
 let pontuacaoPessoa = 0;
 let pontuacaoMaquina = 0;
 
-const resultado = document.getElementById("resultado");
-
-
-
 const jogadaM = () => {
-  const escolhaM = ["Pedra", "Papel", "Tesoura"];
+  const escolhaM = ["Pedra","Papel","Tesoura"];
   return escolhaM[Math.floor(Math.random() * escolhaM.length)].toLowerCase();
 };
-
-const vitoriaM = () => {
-  return ++pontuacaoMaquina + resultado.innerText("A maquina venceu");
-};
-const vitoriaP = () => {
-  return ++pontuacaoPessoa + resultado.innerText("Voce venceu");
-}
-
-
-
-
 const rodada = () => {
-  const botao=document.querySelector("#botao")
-  botao.addEventListener("click",rodada)
-
-  const jogadaP = document.querySelectorAll("input[name='opcao']:checked").value.toLowerCase();
-  
-  
-  switch (jogadaP && jogadaM()) {
-    case (jogadaP === jogadaM()):
-      resultado.innerText("O jogo deu empate");
-
-      break;
-
-    case (jogadaP === "pedra" && jogadaM() === "tesoura"):
-      vitoriaP();
-
-      break;
-
-    case (jogadaP === "papel" && jogadaM() === "pedra"):
-      vitoriaP();
-
-      break;
-    case (jogadaP === "tesoura" && jogadaM() === "papel"):
-      vitoriaP();
-
-      break;
-    case (jogadaM() === "pedra" && jogadaP === "tesoura"):
-      vitoriaM();
-      break;
-    case (jogadaM() === "papel" && jogadaP === "pedra"):
-      vitoriaM();
-      break;
-    case (jogadaM() === "tesoura" && jogadaP === "papel"):
-      vitoriaM();
-      break;
-
+  const resultado = document.getElementById("resultado");
+  const jogadaP = document.querySelector("input[name='opcao']:checked").value;
+  const jogadaAmbos = jogadaP+jogadaM();
+  const empate = jogadaP===jogadaM()
+  if(empate){resultado.innerHTML="O jogo deu empate"
   }
+  switch (jogadaAmbos) {
+    case "pedratesoura":
+      pontuacaoPessoa++;
+      resultado.innerText = "Você Venceu";
+      break;
+    case "papelpedra":
+      pontuacaoPessoa++;
+      resultado.innerText = "Você Venceu"
+      break;
+    case "tesourapapel":
+      pontuacaoPessoa++;
+      resultado.innerText = "Você Venceu";
+      break;
+    case "tesourapedra":
+      pontuacaoMaquina++;
+      resultado.innerText = "A máquina venceu";
+      break;
+    case "pedrapapel":
+      pontuacaoMaquina++;
+      resultado.innerText = "A máquina venceu";
+      break;
+    case "papeltesoura":
+      pontuacaoMaquina++;
+      resultado.innerText = "A máquina venceu";
+  }
+  document.getElementById("score").innerText=`A sua pontuação é:${pontuacaoPessoa} A pontuação da maquina é ${pontuacaoMaquina}`;
 };
+const botao = document.querySelector("#botao");
+botao.addEventListener("click", rodada);
